0f2afa646e4125c25fa6bcf5dcb327b8f5c1d6dd
bibek <cbibek45@gmail.com>
2026-07-21 14:04:33 +0545

    Add loop exercise

--- Week-1/basic exercise.py
+++ Week-1/basic exercise.py
@@ -41,14 +41,14 @@ for fruit in reversed(fruits):
 ## Dictionaries
 
 
-# 6: Store a student's details in a dictionary and print them
+# 5: Store a student's details in a dictionary and print them
 student = {"name": "Alex", "age": 20, "course": "AI/ML"}
 print("Name:", student["name"])
 print("Age:", student["age"])
 print("Course:", student["course"])
 
 
-# 7: Count how many times each word appears in a list
+# 6: Count how many times each word appears in a list
 words = ["apple", "banana", "apple", "cherry", "banana", "apple"]
 word_count = {}
 for word in words:
@@ -59,14 +59,14 @@ for word in words:
 print(word_count)
 
 
-# 8: Add a new key-value pair to a dictionary and update an existing one
+# 7: Add a new key-value pair to a dictionary and update an existing one
 person = {"name": "Sam", "city": "Delhi"}
 person["age"] = 22          # add new key
 person["city"] = "Mumbai"   # update existing key
 print(person)
 
 
-# 9: Check whether age exists or not 
+# 8: Check whether age exists or not 
 student = {
     "name":"Bibek",
     "course":"CS"
@@ -77,4 +77,30 @@ if "age" in student:
 else:
     print("Not Found")
 
+
+
+#### LOOP
+
+# 9: Print numbers from 1 to 10 using a loop
+for i in range(1, 11):
+    print(i)
+
+
+# 10: print all even numbers from 1 to 20 using a loop
+for i in range(1, 21):
+    if i % 2==0:
+        print(i)
+
+
+# 11: Print the multiplication table of a given number using a loop
+number = 5
+for i in range(1, 11):
+    print(f"{number} x {i} = {number * i}")
+
+# 12: Count down from 10 to 1, then print "finished"
+count = 10
+while count > 0:
+    print(count)
+    count -= 1
+print("Finished!")     
 
